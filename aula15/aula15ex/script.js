d2dd6946c8608d5331d59ccc3d6593653f53175c
marianakorb <mariana.korb.mk@gmail.com>
2022-12-13 10:04:25 -0300

    aula 15

---
+++ aula15/aula15ex/script.js
@@ -0,0 +1,22 @@
+const num = Number(prompt('Digite um número: '))
+
+let numeroTitulo = document.getElementById('numero-titulo')
+let item = document.getElementById('lista')
+
+numeroTitulo.innerHTML += ` ${num}`
+
+
+let raiz = num ** (1/2)
+let inteiro = Number.isInteger(num)
+let notANumber = Number.isNaN(num)
+let toDown = Math.floor(num)
+let toUp = Math.ceil(num)
+let duasCasas = Number(num.toFixed(2))
+
+item.innerHTML = `<p>A raiz quadrada é: ${raiz}</p>`
+item.innerHTML += `<p>É inteiro? ${inteiro}</p>`
+item.innerHTML += `<p>É NaN? ${notANumber}</p>`
+item.innerHTML += `<p>Arredondado para baixo: ${toDown}</p>`
+item.innerHTML += `<p>Arredondado para cima: ${toUp}</p>`
+item.innerHTML += `<p>Com duas casas decimais: ${duasCasas}</p>`
+
---
+++ aula15/index.js
@@ -0,0 +1,19 @@
+// Gerando um número inteiro aletório entre 0 e 10
+const aleatorio = Math.round(Math.random() * 10)
+console.log(aleatorio)
+
+// Gerando a raiz quadrada de um número
+console.log(169 ** (1/2))
+
+// Número dividido por zero gera infinity(booleano)
+console.log(!!(8/0))
+
+let num = 8.74
+let n = Math.floor(num) // arredonda para baixo
+let n1 = Math.ceil(num) // arredonda para cima
+let n2 = Math.round(num) // arredonda para o mais próximo
+let n3 = Math.max(10, 5, 2, 87, -5) // retorna o maior número
+let n4 = Math.min(10, 5, 2, 87, -5) // retorna o menor número
+let n5 = Math.random() // gera um número aleatório entre 0 e 1
+
+console.log(n5)
